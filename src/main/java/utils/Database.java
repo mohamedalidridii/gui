bbed3e37db1677f36682db844458a6064876b26f
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2025-04-19 20:02:47 +0100

    first commit

---
+++ src/main/java/utils/Database.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    final String URl="jdbc:mysql://localhost:3306/gui";
+    final String USERNAME="root";
+    final String PASSWORD="root";
+    Connection connection;
+
+    static Database instance;
+    private Database(){
+        try {
+            connection= DriverManager.getConnection(URl,USERNAME,PASSWORD);
+            System.out.println("Connected to database");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    public  static Database getInstance(){
+        if(instance==null)
+            instance=new Database();
+        return instance;
+
+    }
+
+}
+
