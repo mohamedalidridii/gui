9c03579a9dfa107ece71888a71d88b6385b800e8
mohamedalidridii <mohamedali.dridi@esprit.tn>
2025-04-20 00:47:33 +0100

    adding product service and product service

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -16,6 +16,39 @@ public class Product {
     }
 
 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getQt() {
+        return qt;
+    }
+
+    public void setQt(int qt) {
+        this.qt = qt;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
--- src/main/java/services/ServiceProduct.java
+++ src/main/java/services/ServiceProduct.java
@@ -14,22 +14,69 @@ public class ServiceProduct implements Iservice<Product> {
     }
     @Override
     public void ajouter(Product product) throws SQLException {
+        System.out.println("in ajouter");
+        try{
+            System.out.println("in try ajouter");
+            String req ="INSERT INTO `product` (`nom`, `desc`, `qt`) VALUES ('"+product.getNom()+"','"+product.getDesc()+"',"+product.getQt()+")";
+            System.out.println("after insert");
+            Statement statement=connection.createStatement();
+            System.out.println("after create stat");
+            statement.executeUpdate(req);
+            System.out.println("Produit ajouté");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void supprimer(Product product) throws SQLException {
+        String req = "DELETE FROM `product` WHERE `id` = ?";
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement(req)) {
+            preparedStatement.setInt(1, product.getId());
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println(" supprimée avec succès");
+            } else {
+                System.out.println("Aucun product trouvée avec l'ID : " + product.getId());
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur lors de la suppression : " + e.getMessage());
+        }
     }
 
     @Override
     public void modifier(Product product) throws SQLException {
-
+        String req = "update product set `nom`=?, `desc`=?, `qt`=? where `id`=?";
+        PreparedStatement preparedStatement = connection.prepareStatement(req);
+        preparedStatement.setString(1, product.getNom());
+        preparedStatement.setString(2, product.getDesc());
+        preparedStatement.setInt(3, product.getQt());
+        preparedStatement.setInt(4, product.getId());
+        preparedStatement.executeUpdate();
+        System.out.println("produit modified");
 
     }
 
     @Override
     public List<Product> afficher() throws SQLException {
-        return List.of();
-    }
+        List<Product> products = new ArrayList<>();
+        String req = "SELECT * FROM `product`\n";
+        Statement statement = connection.createStatement();
 
+        ResultSet rs = statement.executeQuery(req);
+        while (rs.next()) {
+            Product product = new Product();
+            product.setId(rs.getInt(1));
+            product.setNom(rs.getString("nom"));
+            product.setDesc(rs.getString("desc"));
+            product.setQt(rs.getInt(4));
+            products.add(product);
+
+        }
+
+
+        return products;
+    }
 }
